4ee62c2aa09b1b93e3ad4c559fcecdf75a7eef5f
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2023-02-07 00:26:24 -0500

    Make application name generic

--- src/main/java/org/tuttleware/addressbook/LabApplication.java
+++ src/main/java/org/tuttleware/addressbook/LabApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab4Application {
+public class LabApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Lab4Application.class, args);
+        SpringApplication.run(LabApplication.class, args);
     }
 }
